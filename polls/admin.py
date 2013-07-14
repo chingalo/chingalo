1e6c7d83e6581280165dca3acbbcf788d5df4204
chingalo <profschingalo@gmail.com>
2013-07-14 17:17:47 +0300

    show and hide on adminsite

--- polls/admin.py
+++ polls/admin.py
@@ -3,8 +3,8 @@ from polls.models import Poll, Choice
 from django.contrib import admin
 
 class PollAdmin(admin.ModelAdmin):
-    fieldsets = [('type of question', {'fields':['question']}),('date infos',{'fields':['pub_date']})]
+    fieldsets = [('type of question', {'fields':['question'],"classes":['collapse']}),('date infos',{'fields':['pub_date'],"classes":['collapse']})]
 class ChoiceAdmin(admin.ModelAdmin):
-	fieldsets = [('No of votes',{'fields':['vote']}),('Your choice',{'fields':['choice_text']}), (None,{'fields':['poll']})]
+	fieldsets = [('No of votes',{'fields':['vote'],"classes":['collapse']}),('Your choice',{'fields':['choice_text'],"classes":['collapse']}), (None,{'fields':['poll']})]
 admin.site.register(Poll, PollAdmin)
 admin.site.register(Choice, ChoiceAdmin)
