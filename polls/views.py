650c4c0a8eb23206491b48fb9de9929de4e599f6
chingalo <profschingalo@gmail.com>
2013-07-16 11:51:00 +0300

    commrnts on views.py

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from polls.models import Poll, Choice
+#function to view polls available on database
 def index(request):
 	qns = Poll.objects.all()
 	template = loader.get_template('polls/index.html')
@@ -8,13 +9,15 @@ def index(request):
 	return HttpResponse(template.render(context)) 
 
 
-    
+ #function to view choices for correponding poll   
 def detail(request, poll_id):
 	p=Poll.objects.get(id=poll_id)
 	det=p.choice_set.all()
 	template = loader.get_template('polls/details.html')
 	context =RequestContext(request,{'det':det})
 	return HttpResponse(template.render(context))
+
+#function to remove a given poll	
 def delete(request, poll_id):
 	rempoll=Poll.objects.get(id=poll_id)
 	rempoll.delete()
