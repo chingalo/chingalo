6a4ea08c20f7f6f1448588f097b82c557b89ed85
chingalo <profschingalo@gmail.com>
2013-07-17 14:44:57 +0300

    simplificatons on views.py and ulrs in templates

--- polls/urls.py
+++ polls/urls.py
@@ -5,7 +5,7 @@ from polls import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='view all polls'),
     url(r'^createpoll/$', views.create_poll, name='create_poll'),
-    url(r'^(?P<poll_id>\d+)/delete/$', views.delete, name= 'delete a poll'),    
-    url(r'^(?P<poll_id>\d+)/$', views.detail, name='choice of given poll'),
-    url(r'^createchoice/$',views.create_choice, name=' create choice for a given poll'),
+    url(r'^(?P<poll_id>\d+)/delete/$', views.delete, name= 'delete_poll'),    
+    url(r'^(?P<poll_id>\d+)/$', views.detail, name='choice_view'),
+    url(r'^createchoice/$',views.create_choice, name='create_choice'),
 )
--- polls/views.py
+++ polls/views.py
@@ -1,36 +1,32 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import RequestContext, loader
+from django.http import HttpResponseRedirect
 from polls.models import Poll, Choice
 from polls.forms import Create_poll, Create_choice
 from django.shortcuts import render_to_response
 from django.core.context_processors import csrf
-
+from django.shortcuts import render
 
 #function to view polls available on database
 def index(request):
 	qns = Poll.objects.all()
-	choices = Choice.objects.all()
-	template = loader.get_template('polls/index.html')
-	context = RequestContext(request,{'qns':qns,'choices':choices})   
-	return HttpResponse(template.render(context)) 
+	choices = Choice.objects.all()	
+	context ={'qns':qns,'choices':choices}   
+	return render(request, 'polls/index.html', context) 
 
 
  #function to view choices for correponding poll   
 def detail(request, poll_id):
 	p=Poll.objects.get(id=poll_id)
-	det=p.choice_set.all()
-	template = loader.get_template('polls/details.html')
-	context =RequestContext(request,{'det':det})
-	return HttpResponse(template.render(context))
+	det=p.choice_set.all()	
+	context ={'det':det}
+	return render(request,'polls/details.html',context)
 
 #function to remove a given poll	
 def delete(request, poll_id):
 	rempoll=Poll.objects.get(id=poll_id)
 	rempoll.delete()
-	word='wow you have delete item'
-	template = loader.get_template('polls/message.html')
-	context =RequestContext(request,{'word':word})
-	return HttpResponse(template.render(context))
+	word='wow you have delete item'	
+	context ={'word':word}
+	return render(request,'polls/message.html',context)
 	
 #to add poll
 def create_poll(request):
@@ -45,7 +41,7 @@ def create_poll(request):
 		 args.update(csrf(request))
 		 args[ 'form' ] = form
 		 return render_to_response('polls/newpoll.html',args)
-		 
+#to add choice on a given poll		 
 def create_choice(request):
 	if request.POST:
 	 form = Create_choice(request.POST)
